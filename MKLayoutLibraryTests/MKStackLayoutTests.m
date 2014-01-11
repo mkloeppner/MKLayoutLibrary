0a226b64befe433745e2a7484e9040c947cef095
mkloeppner <mkloeppner@me.com>
2014-01-11 20:19:11 +0100

    Added tests for stack layout

--- MKLayoutLibraryTests/MKStackLayoutTests.m
+++ MKLayoutLibraryTests/MKStackLayoutTests.m
@@ -79,8 +79,8 @@ describe(@"MKStackLayout", ^{
         
         expect(0.0f).to.equal(subview1.frame.origin.x + 5.0f);
         expect(0.0f).to.equal(subview1.frame.origin.y + 5.0f);
-        expect(container.frame.size.width).to.equal(subview1.frame.size.width - 10.0f);
-        expect(container.frame.size.height).to.equal(subview1.frame.size.height - 10.0f);
+        expect(container.frame.size.width - 10.0f).to.equal(subview1.frame.size.width);
+        expect(container.frame.size.height - 10.0f).to.equal(subview1.frame.size.height);
         
     });
     
@@ -96,18 +96,65 @@ describe(@"MKStackLayout", ^{
         
         expect(0.0f).to.equal(subview1.frame.origin.x + 10.0f);
         expect(0.0f).to.equal(subview1.frame.origin.y + 10.0f);
-        expect(container.frame.size.width).to.equal(subview1.frame.size.width - 20.0f);
-        expect(container.frame.size.height).to.equal(subview1.frame.size.height - 20.0f);
+        expect(container.frame.size.width - 20.0f).to.equal(subview1.frame.size.width);
+        expect(container.frame.size.height - 20.0f).to.equal(subview1.frame.size.height);
         
-        expect(0.0f).to.equal(subview2.frame.origin.x + 15.0f);
-        expect(0.0f).to.equal(subview2.frame.origin.y + 15.0f);
-        expect(container.frame.size.width).to.equal(subview2.frame.size.width - 30.0f);
-        expect(container.frame.size.height).to.equal(subview2.frame.size.height - 30.0f);
+        expect(0.0f + 15.0f).to.equal(subview2.frame.origin.x);
+        expect(0.0f + 15.0f).to.equal(subview2.frame.origin.y);
+        expect(container.frame.size.width - 30.0f).to.equal(subview2.frame.size.width);
+        expect(container.frame.size.height - 30.0f).to.equal(subview2.frame.size.height);
         
         // draw order
         expect(layout.items.count).to.equal(2);
         expect(subview1).to.equal([layout.items[0] view]);
         expect(subview2).to.equal([layout.items[1] view]);
+        
+    });
+    
+    it(@"should layout sublayouts in stack layout", ^{
+        
+        MKStackLayout *sublayout = [[MKStackLayout alloc] initWithView:container];
+        [sublayout addSubview:subview1];
+        [layout addSublayout:sublayout];
+        
+        expect(0.0f).to.equal(subview1.frame.origin.x);
+        expect(0.0f).to.equal(subview1.frame.origin.y);
+        expect(container.frame.size.width).to.equal(subview1.frame.size.width);
+        expect(container.frame.size.height).to.equal(subview1.frame.size.height);
+        
+    });
+    
+    it(@"should apply margin of sublayout", ^{
+        
+        MKStackLayout *sublayout = [[MKStackLayout alloc] initWithView:container];
+        MKLayoutItem *layoutItem1 = [sublayout addSubview:subview1];
+        layoutItem1.margin = UIEdgeInsetsMake(10.0f, 10.0f, 10.0f, 10.0f);
+        
+        [layout addSublayout:sublayout];
+        
+        expect(0.0f + 10.0f).to.equal(subview1.frame.origin.x);
+        expect(0.0f + 10.0f).to.equal(subview1.frame.origin.y);
+        expect(container.frame.size.width - 20.0f).to.equal(subview1.frame.size.width);
+        expect(container.frame.size.height - 20.0f).to.equal(subview1.frame.size.height);
+        
+    });
+    
+    it(@"should apply margin with a depth of hirarchy of two", ^{
+        
+        MKStackLayout *stackLayout = [[MKStackLayout alloc] initWithView:container];
+        MKLayoutItem *layoutItem1 = [stackLayout addSubview:subview1];
+        MKLayoutItem *layoutItem2 = [layout addSublayout:stackLayout];
+        
+        layoutItem1.margin = UIEdgeInsetsMake(10.0f, 10.0f, 10.0f, 10.0f);
+        layoutItem2.margin = UIEdgeInsetsMake(15.0f, 15.0f, 15.0f, 15.0f);
+        
+        [layout layout];
+        
+        expect(0.0f + 10.0f + 15.0f).to.equal(subview1.frame.origin.x);
+        expect(0.0f + 10.0f + 15.0f).to.equal(subview1.frame.origin.y);
+        expect(container.frame.size.width - 20.0f - 30.0f).to.equal(subview1.frame.size.width);
+        expect(container.frame.size.height - 20.0f - 30.0f).to.equal(subview1.frame.size.height);
+        
         
     });
     
