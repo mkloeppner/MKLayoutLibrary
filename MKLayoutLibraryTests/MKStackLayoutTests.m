8c922da351eed77c15198a0c4ea4a9d639de769d
mkloeppner <mkloeppner@me.com>
2014-01-11 20:05:23 +0100

    Implemented tests for stack layout

--- MKLayoutLibraryTests/MKStackLayoutTests.m
+++ MKLayoutLibraryTests/MKStackLayoutTests.m
@@ -34,7 +34,7 @@ describe(@"MKStackLayout", ^{
     });
     
     it(@"should stack a view", ^{
-       
+        
         [layout addSubview:subview1];
         
         [layout layout];
@@ -62,6 +62,47 @@ describe(@"MKStackLayout", ^{
         expect(0.0f).to.equal(subview2.frame.origin.y);
         expect(container.frame.size.width).to.equal(subview2.frame.size.width);
         expect(container.frame.size.height).to.equal(subview2.frame.size.height);
+        
+        // draw order
+        expect(layout.items.count).to.equal(2);
+        expect(subview1).to.equal([layout.items[0] view]);
+        expect(subview2).to.equal([layout.items[1] view]);
+        
+    });
+    
+    it(@"should insert a margin for a view its specified", ^{
+        
+        MKLayoutItem *layoutItem1 = [layout addSubview:subview1];
+        layoutItem1.margin = UIEdgeInsetsMake(5.0f, 5.0f, 5.0f, 5.0f);
+        
+        [layout layout];
+        
+        expect(0.0f).to.equal(subview1.frame.origin.x + 5.0f);
+        expect(0.0f).to.equal(subview1.frame.origin.y + 5.0f);
+        expect(container.frame.size.width).to.equal(subview1.frame.size.width - 10.0f);
+        expect(container.frame.size.height).to.equal(subview1.frame.size.height - 10.0f);
+        
+    });
+    
+    it(@"should insert different margins for different views", ^{
+        
+        MKLayoutItem *layoutItem1 = [layout addSubview:subview1];
+        MKLayoutItem *layoutItem2 = [layout addSubview:subview2];
+        
+        layoutItem1.margin = UIEdgeInsetsMake(10.0f, 10.0f, 10.0f, 10.0f);
+        layoutItem2.margin = UIEdgeInsetsMake(15.0f, 15.0f, 15.0f, 15.0f);
+        
+        [layout layout];
+        
+        expect(0.0f).to.equal(subview1.frame.origin.x + 10.0f);
+        expect(0.0f).to.equal(subview1.frame.origin.y + 10.0f);
+        expect(container.frame.size.width).to.equal(subview1.frame.size.width - 20.0f);
+        expect(container.frame.size.height).to.equal(subview1.frame.size.height - 20.0f);
+        
+        expect(0.0f).to.equal(subview2.frame.origin.x + 15.0f);
+        expect(0.0f).to.equal(subview2.frame.origin.y + 15.0f);
+        expect(container.frame.size.width).to.equal(subview2.frame.size.width - 30.0f);
+        expect(container.frame.size.height).to.equal(subview2.frame.size.height - 30.0f);
         
         // draw order
         expect(layout.items.count).to.equal(2);
