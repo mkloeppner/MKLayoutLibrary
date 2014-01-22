9dbd6671f9daa6ae7fb54c73cf13792ef05acbd7
mkloeppner <mkloeppner@me.com>
2014-01-22 08:14:37 +0100

    Support both, absolute and relative sizing

--- MKLayoutLibrary/MKLinearLayout.m
+++ MKLayoutLibrary/MKLinearLayout.m
@@ -8,6 +8,13 @@
 
 #import "MKLinearLayout.h"
 
+@interface MKLinearLayout ()
+
+// Transient, outdated after layout
+@property (assign, nonatomic) CGRect bounds;
+
+@end
+
 @implementation MKLinearLayout
 
 - (instancetype)initWithView:(UIView *)view
@@ -35,36 +42,40 @@
 
 - (void)layoutBounds:(CGRect)bounds
 {
+    self.bounds = bounds;
+    
     float currentPos = 0.0f;
     float overallWeight = 0.0f;
-    float overallPoints = 0.0f;
-    float contentSize = self.orientation == MKLinearLayoutOrientationHorizontal ? bounds.size.width : bounds.size.height;
+    float overallLength = 0.0f;
+    
+    [self calculateOverallWeight:&overallWeight overallLength:&overallLength];
+    
+    float contentLength = [self lengthForOrientation:self.orientation];
     
     for (int i = 0; i < self.items.count; i++) {
+        
         MKLinearLayoutItem *item = self.items[i];
-        if (item.weight != kMKLinearLayoutWeightInvalid) {
-            overallWeight += item.weight;
+        
+        CGRect rect = CGRectMake(0.0f, 0.0f, 0.0f, 0.0f);
+        
+        // Apply current position]
+    
+        if (self.orientation == MKLinearLayoutOrientationHorizontal) {
+            rect.origin.x = currentPos;
         } else {
-            overallPoints += [self pointsForOrientationFromItem:item];
-        }
-    }
-    
-    for (int i = 0; i < self.items.count; i++) {
-        
-        MKLinearLayoutItem *item = self.items[i];
-        
-        float currentStep = [self pointsForOrientationFromItem:item];
-        if (item.weight != kMKLinearLayoutWeightInvalid) {
-            float percent = item.weight / overallWeight;
-            
-            float boundsWithoutAbsoluteSizes = contentSize - overallPoints;
-            currentStep = boundsWithoutAbsoluteSizes * percent;
+            rect.origin.y = currentPos;
         }
         
-        CGRect rect = [self orientedRectForPosition:currentPos length:currentStep bounds:bounds];
+        rect.size.width = [self lengthForItem:item orientation:MKLinearLayoutOrientationHorizontal overallWeight:overallWeight overallLength:overallLength contentLength:contentLength];
+        rect.size.height = [self lengthForItem:item orientation:MKLinearLayoutOrientationVertical overallWeight:overallWeight overallLength:overallLength contentLength:contentLength];
+        
+        // Apply offset for recursive layout calls in order to achieve sublayouts
         rect.origin.x += bounds.origin.x;
         rect.origin.y += bounds.origin.y;
         
+        currentPos += [self lengthFromRect:rect orientation:self.orientation];
+        
+        // Apply the margin
         rect = UIEdgeInsetsInsetRect(rect, item.margin);
         
         if (item.subview) {
@@ -72,37 +83,66 @@
         } else if (item.sublayout) {
             [item.sublayout layoutBounds:rect];
         }
-        
-        currentPos += currentStep;
-    }
-}
-
-- (CGFloat)pointsForOrientationFromItem:(MKLinearLayoutItem *)item
-{
-    return self.orientation == MKLinearLayoutOrientationHorizontal ? item.size.width : item.size.height;
-}
-
-/**
- * Get the rect for the actual orientation
- */
-- (CGRect)orientedRectForPosition:(CGFloat)position length:(CGFloat)length bounds:(CGRect)bounds
-{
-    CGRect rect;
     
-    if (self.orientation == MKLinearLayoutOrientationHorizontal) {
-        rect.origin.x = position;
-        rect.origin.y = 0.0f;
-        rect.size.width = length;
-        rect.size.height = bounds.size.height;
-    } else if (self.orientation == MKLinearLayoutOrientationVertical) {
-        rect.origin.x =  0.0f;
-        rect.origin.y = position;
-        rect.size.width = bounds.size.width;
-        rect.size.height = length;
-    } else {
-        [NSException raise:@"InvalidArgumentException" format:@"property \"orientation\" got an invalid value"];
     }
-    return rect;
+    
+    self.bounds = CGRectMake(0.0f, 0.0f, 0.0f, 0.0f);
+}
+
+- (CGFloat)lengthForItem:(MKLinearLayoutItem *)item orientation:(MKLinearLayoutOrientation)orientation overallWeight:(CGFloat)overallWeight overallLength:(CGFloat)overallLength contentLength:(CGFloat)contentLength
+{
+    float itemLength = [self pointsForOrientation:orientation fromItem:item];
+    if (orientation == self.orientation) {
+        if (item.weight != kMKLinearLayoutWeightInvalid) {
+            float percent = item.weight / overallWeight;
+            
+            float boundsWithoutAbsoluteSizes = contentLength - overallLength;
+            itemLength = boundsWithoutAbsoluteSizes * percent;
+        }
+    }
+    return itemLength;
+}
+
+- (void)calculateOverallWeight:(CGFloat *)overallWeight overallLength:(CGFloat *)overallPoints
+{
+    for (int i = 0; i < self.items.count; i++) {
+        MKLinearLayoutItem *item = self.items[i];
+        if (item.weight != kMKLinearLayoutWeightInvalid) {
+            *overallWeight += item.weight;
+        } else {
+            *overallPoints += [self pointsForOrientation:self.orientation fromItem:item];
+        }
+    }
+}
+
+// TODO: Return the real points. item.size can contain flags as match_parent
+- (CGFloat)pointsForOrientation:(MKLinearLayoutOrientation)orientation fromItem:(MKLinearLayoutItem *)item
+{
+    CGFloat points =  orientation == MKLinearLayoutOrientationHorizontal ? item.size.width : item.size.height;
+    if (points == kMKLinearLayoutSizeValueMatchParent) {
+        points = [self lengthForOrientation:orientation];
+    }
+    return points;
+}
+
+- (CGFloat)lengthForOrientation:(MKLinearLayoutOrientation)orientation
+{
+    return [self lengthFromRect:self.bounds orientation:orientation];
+}
+
+- (CGFloat)lengthFromRect:(CGRect)rect orientation:(MKLinearLayoutOrientation)orientation
+{
+    switch (orientation) {
+        case MKLinearLayoutOrientationHorizontal:
+            return rect.size.width;
+            break;
+        case MKLinearLayoutOrientationVertical:
+            return rect.size.height;
+            
+        default:
+            break;
+    }
+    [NSException raise:@"Unknown state exception" format:@"Can't calculate the length for orientation %i", orientation];
 }
 
 @end
